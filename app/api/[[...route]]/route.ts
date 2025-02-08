d93309f4bd661ac9d77ba3b963c0255110918390
Eudes-Dev <euudesdev@gmail.com>
2025-02-08 00:31:30 +0000

    Accounts GET API

--- app/api/[[...route]]/route.ts
+++ app/api/[[...route]]/route.ts
@@ -2,14 +2,26 @@ import { z } from 'zod'
 import { zValidator } from "@hono/zod-validator";
 import { Hono } from 'hono'
 import { handle } from 'hono/vercel'
+import accounts from "@/app/api/[[...route]]/accounts";
+import {HTTPException} from "hono/http-exception";
 
 export const runtime = 'edge'
 
 const app = new Hono().basePath('/api')
 
-app.get('/hello', (c) => {
-    return c.json({ hello: 'World' })
+app.onError((err, c) => {
+    if (err instanceof HTTPException) {
+        return err.getResponse()
+    }
+
+    return c.json({ error: 'Internal Server Error' }, 500)
 })
+
+const routes = app
+    .route('/accounts', accounts)
 
 export const GET = handle(app)
 export const POST = handle(app)
+
+
+export type AppType = typeof routes
